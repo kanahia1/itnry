3fbacb47cc5e9e7a86d1aa052d4a5b43dcf8ffb9
kanahia1 <kaushalkanahia@gmail.com>
2024-09-01 00:35:40 +0530

    Loader Animation Added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import '@/styles/globals.css';
 import { Poppins } from 'next/font/google';
+import { Loader } from '@/components';
 
 const Poppins_ = Poppins({
     style: 'normal',
@@ -11,9 +12,32 @@ const Poppins_ = Poppins({
 });
 export default function App({ Component, pageProps }) {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        let timer;
+        const handleComplete = () => (timer = setTimeout(() => setLoading(false), 1000));
+
+        const handleStart = () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+            setLoading(true);
+        };
+
+        router.events.on('routeChangeStart', handleStart);
+        router.events.on('routeChangeComplete', handleComplete);
+        router.events.on('routeChangeError', handleComplete);
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart);
+            router.events.off('routeChangeComplete', handleComplete);
+            router.events.off('routeChangeError', handleComplete);
+        };
+    }, [router.events]);
     return (
         <main className={Poppins_.className}>
-            <Component {...pageProps} />
+            {loading ? <Loader /> : <Component {...pageProps} />}
         </main>
     );
 }
