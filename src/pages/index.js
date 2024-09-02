997b28bcb4e46991c1b857451e10e3dd4a582215
kanahia1 <kaushalkanahia@gmail.com>
2024-09-03 02:21:28 +0530

    Changed Background to Video

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,13 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <>
-            <div className={styles.background}></div>
+            <div className={styles.background}>
+                <video autoPlay muted loop className={styles.myVideo}>
+                    <source src="/home/landing_bg.mp4" type="video/mp4" />
+                </video>
+            </div>
             <div className={styles.header}>
-                <Image src="/home/itnry.svg" width={120} height={60} />
+                <Image alt="itnry" src="/home/itnry.svg" width={120} height={60} />
                 <Link href={'https://in.linkedin.com/in/kanahia-9850bb253'} target="_blank">
                     LinkedIn
                 </Link>
@@ -22,20 +26,20 @@ export default function Home() {
                 </p>
                 <Link href="/planner" className={styles.button}>
                     <div>
-						Create itinerary
-						<svg
-							width="36"
-							height="16"
-							viewBox="-10 0 16 16"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z"
-								fill="#555555"
-							/>
-						</svg>
-					</div>
+                        Create itinerary
+                        <svg
+                            width="36"
+                            height="16"
+                            viewBox="-10 0 16 16"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z"
+                                fill="#555555"
+                            />
+                        </svg>
+                    </div>
                 </Link>
             </div>
         </>
