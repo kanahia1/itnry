9a1af909060f61679226cacce5bf76c31321e3a5
kanahia1 <kaushalkanahia@gmail.com>
2024-09-03 14:14:06 +0530

    Made changes to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
                         >
                             <path
                                 d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z"
-                                fill="#555555"
+                                fill="#000000"
                             />
                         </svg>
                     </div>
